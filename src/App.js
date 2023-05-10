6fbedf4253652fb5f6332e65776e61fd490d57dc
zohir222 <bensalemzohir284@gmail.com>
2023-05-10 19:37:18 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [dd , setdd] = useState();
+  useEffect(()=>{
+      axios.get("https://testbackend-production-94a2.up.railway.app/books").then((Response)=>{
+        setdd(Response.data) ;
+      })
+      },[]) ;
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      { dd.map( (item)=> {return <h2 key={item} > {item} </h2> } ) }
     </div>
   );
 }
